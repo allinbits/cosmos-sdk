e123c3b244cddce3d9d4660ef399ec51fcb00e0a
Timothy Chen <tnachen@gmail.com>
2019-12-19 06:16:57 +0000

    Fix script

--- grid_search.py
+++ grid_search.py
@@ -2,30 +2,33 @@
 
 import os
 import subprocess
+import time
+from threading import Thread
 
 import distutils.spawn
 psrecord_path = distutils.spawn.find_executable("psrecord")
 
-for every in range(1, 501, 100):
-    for recent in range(0, 201, 50):
-        os.environ["KEEP_EVERY"] = str(every)
-        os.environ["KEEP_RECENT"] = str(recent)
+def launch_benchmark(every, recent, filename):
+    os.environ["KEEP_EVERY"] = str(every)
+    os.environ["KEEP_RECENT"] = str(recent)
+    output = subprocess.check_output(["make", "test-sim-benchmark"])
+    with open("{}.results".format(filename), 'a') as log:
+        log.write(output)
+
+for every in range(1, 201, 50):
+    for recent in range(100, 101, 100):
         filename = "every-{}-recent-{}".format(str(every), str(recent))
         print("Running {}".format(filename))
-        log = open("{}.results".format(filename), 'a')
 
-        env = {
-            "KEEP_EVERY": str(every),
-            "KEEP_RECENT": str(recent),
-            "PATH": os.environ["PATH"],
-            "HOME": "/home/ubuntu"
-        }
-        #process = subprocess.Popen(["make", "test-sim-benchmark"], shell=True, stdout=log, stderr=log, env=env)
-        output = subprocess.check_output(["make", "test-sim-benchmark"])
-        log.write(output)
-        #pid = process.pid
-        #process_pid = subprocess.check_output("ps -ef | grep simapp.test | grep -v grep | awk '{print $2}'", stderr=subprocess.STDOUT, shell=True).strip().decode("utf-8")
-        #print(process_pid)
-        #os.system("psrecord {} --log {}.log --interval 15".format(process_pid, filename))
-        #os.system("{} {} --log {}.log".format(psrecord_path, str(pid), filename))
-        log.close()
+        # Some reason popen doesn't work, so we have to use check_output which waits until the command exits.
+        # Therefore putting this in a separate thread.
+        Thread(target=(lambda: launch_benchmark(every, recent, filename))).start()
+
+        # Adding sleep here to wait until the benchmark command is ran from make
+        time.sleep(5)
+
+        process_pid = subprocess.check_output("ps -ef | grep simapp.test | grep -v grep | awk '{print $2}'", stderr=subprocess.STDOUT, shell=True).strip().decode("utf-8")
+        print("Process pid")
+        print(process_pid)
+        os.system("psrecord {} --log {}.log --interval 15".format(process_pid, filename))
+        os.system("{} {} --log {}.log".format(psrecord_path, str(pid), filename))
