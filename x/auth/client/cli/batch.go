3623a6a476ee034819c279395c1f5753df947bdb
Jonathan Gimeno <jgimeno@gmail.com>
2020-05-20 20:50:21 +0200

    create multi sign key

--- x/auth/client/cli/batch.go
+++ x/auth/client/cli/batch.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"fmt"
 
+	keybase "github.com/cosmos/cosmos-sdk/crypto/keys"
+
 	"github.com/pkg/errors"
 
 	"github.com/spf13/cobra"
@@ -33,9 +35,18 @@ This command is intended to work offline for security purposes.`,
 
 func makeBatchSignCmd(cdc *codec.Codec) func(cmd *cobra.Command, args []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		_, err := keys.NewKeyBaseFromDir(viper.GetString(flags.FlagHome))
+		kb, err := keys.NewKeyBaseFromDir(viper.GetString(flags.FlagHome))
 		if err != nil {
 			return err
+		}
+
+		multisigInfo, err := kb.Get(viper.GetString(flags.FlagFrom))
+		if err != nil {
+			return err
+		}
+
+		if multisigInfo.GetType() != keybase.TypeMulti {
+			return fmt.Errorf("%q must be of type %s: %s", args[1], keybase.TypeMulti, multisigInfo.GetType())
 		}
 
 		txsToSign, err := utils.ReadStdTxsFromFile(cdc, args[0])
--- x/auth/client/cli/batch_test.go
+++ x/auth/client/cli/batch_test.go
@@ -1,15 +1,21 @@
 package cli
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
+
+	"github.com/tendermint/tendermint/crypto/multisig"
 
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/go-amino"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	keys2 "github.com/cosmos/cosmos-sdk/crypto/keys"
 	"github.com/cosmos/cosmos-sdk/tests"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 func TestGetBatchSignCommand(t *testing.T) {
@@ -19,13 +25,99 @@ func TestGetBatchSignCommand(t *testing.T) {
 	tempDir, cleanFunc := tests.NewTestCaseDir(t)
 	t.Cleanup(cleanFunc)
 
-	viper.Set(flags.FlagHome, tempDir)
+	_, err := createKeybaseWithMultisigAccount(tempDir)
+	require.NoError(t, err)
 
+	viper.Set(flags.FlagHome, tempDir)
+	viper.Set(flags.FlagFrom, "multi")
 	cmd.SetArgs([]string{
 		"./testdata/txs.json",
 		filepath.Join(tempDir, "outputfile"),
 	})
 
-	err := cmd.Execute()
+	err = cmd.Execute()
 	require.NoError(t, err)
+}
+
+func createKeybaseWithMultisigAccount(dir string) ([]crypto.PubKey, error) {
+	kb, err := keys.NewKeyBaseFromDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var pubKeys []crypto.PubKey
+	for i := 0; i < 4; i++ {
+		mnemonic, _, _ := kb.CreateMnemonic(
+			fmt.Sprintf("acc%d", i),
+			keys2.English,
+			"",
+			keys2.Secp256k1,
+		)
+
+		pubKeys = append(pubKeys, mnemonic.GetPubKey())
+	}
+
+	pk := multisig.NewPubKeyMultisigThreshold(2, pubKeys)
+	if _, err := kb.CreateMulti("multi", pk); err != nil {
+		return nil, err
+	}
+
+	return pubKeys, nil
+}
+
+func TestGetBatchSignCommand_Error(t *testing.T) {
+	tests := []struct {
+		name          string
+		errorContains string
+		keybasePrep   func() (cleanFunc func(), tempDir string)
+	}{
+		{
+			name:          "not multisign",
+			errorContains: "must be of type multi",
+			keybasePrep: func() (func(), string) {
+				tempDir, cleanFunc := tests.NewTestCaseDir(t)
+				t.Cleanup(cleanFunc)
+
+				kb, err := keys.NewKeyBaseFromDir(tempDir)
+				require.NoError(t, err)
+
+				_, _, err = kb.CreateMnemonic("acc1", keys2.English, "", keys2.Secp256k1)
+				require.NoError(t, err)
+
+				return cleanFunc, tempDir
+			},
+		},
+		{
+			name:          "non existing key",
+			errorContains: "Key acc1 not found",
+			keybasePrep: func() (func(), string) {
+				tempDir, cleanFunc := tests.NewTestCaseDir(t)
+				t.Cleanup(cleanFunc)
+
+				return cleanFunc, tempDir
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			cdc := amino.NewCodec()
+			cmd := GetBatchSignCommand(cdc)
+
+			cleanFunc, tempDir := tt.keybasePrep()
+			defer cleanFunc()
+
+			viper.Set(flags.FlagHome, tempDir)
+			viper.Set(flags.FlagFrom, "acc1")
+			cmd.SetArgs([]string{
+				"./testdata/txs.json",
+				filepath.Join(tempDir, "outputfile"),
+			})
+
+			err := cmd.Execute()
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tt.errorContains)
+		})
+	}
 }
