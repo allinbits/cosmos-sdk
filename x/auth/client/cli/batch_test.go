484143d4bd188d90dad788fcf14f5fa667cd0ac2
Jonathan Gimeno <jgimeno@gmail.com>
2020-05-21 17:36:43 +0200

    add test with invalid keybase

--- x/auth/client/cli/batch_test.go
+++ x/auth/client/cli/batch_test.go
@@ -96,52 +96,60 @@ func createKeybaseWithMultisigAccount(dir string) (keys2.Keybase, []crypto.PubKe
 }
 
 func TestGetBatchSignCommand_Error(t *testing.T) {
-	tests := []struct {
+	tts := []struct {
 		name          string
 		errorContains string
-		keybasePrep   func() (cleanFunc func(), tempDir string)
+		keybasePrep   func(tempDir string)
 		providedFlags map[string]interface{}
 	}{
 		{
 			name:          "flag multisign not provided",
 			errorContains: "only multisig signature is supported",
-			keybasePrep: func() (func(), string) {
-				tempDir, cleanFunc := tests.NewTestCaseDir(t)
-				t.Cleanup(cleanFunc)
-
+			keybasePrep: func(tempDir string) {
 				kb, err := keys.NewKeyBaseFromDir(tempDir)
 				require.NoError(t, err)
 
 				_, _, err = kb.CreateMnemonic("acc1", keys2.English, "", keys2.Secp256k1)
 				require.NoError(t, err)
-
-				return cleanFunc, tempDir
 			},
 		},
 		{
 			name:          "not existing key",
 			errorContains: "key not found: Key not-existing not found",
-			keybasePrep: func() (func(), string) {
-				tempDir, cleanFunc := tests.NewTestCaseDir(t)
-				t.Cleanup(cleanFunc)
-
-				return cleanFunc, tempDir
+			keybasePrep: func(tempDir string) {
 			},
 			providedFlags: map[string]interface{}{
 				cli.FlagMultisig: "cosmos1pf7m2k50lv0pc27wjz3452vu2xqs8yevxhv7w3",
 				flags.FlagFrom:   "not-existing",
 			},
 		},
+		{
+			name:          "invalid passphrase",
+			errorContains: "invalid account password",
+			keybasePrep: func(tempDir string) {
+				createKeybaseWithMultisigAccount(tempDir)
+			},
+			providedFlags: map[string]interface{}{
+				cli.FlagMultisig: "cosmos1pf7m2k50lv0pc27wjz3452vu2xqs8yevxhv7w3",
+				flags.FlagFrom:   "acc1",
+			},
+		},
 	}
 
-	for _, tt := range tests {
+	cdc := amino.NewCodec()
+	sdk.RegisterCodec(cdc)
+	staking.RegisterCodec(cdc)
+
+	for _, tt := range tts {
 		tt := tt
+		tempDir, cleanFunc := tests.NewTestCaseDir(t)
+
 		t.Run(tt.name, func(t *testing.T) {
-			cdc := amino.NewCodec()
+			defer cleanFunc()
+
 			cmd := cli.GetBatchSignCommand(cdc)
 
-			cleanFunc, tempDir := tt.keybasePrep()
-			defer cleanFunc()
+			tt.keybasePrep(tempDir)
 
 			viper.Reset()
 			viper.Set(flags.FlagHome, tempDir)
