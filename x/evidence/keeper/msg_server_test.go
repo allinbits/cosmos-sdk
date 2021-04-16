0698fa91d2e1e9b6aea6d36d471317579b74d4fb
Jonathan Gimeno <jgimeno@gmail.com>
2021-04-16 16:42:00 +0200

    fix imports

--- x/evidence/keeper/msg_server_test.go
+++ x/evidence/keeper/msg_server_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cosmos/cosmos-sdk/x/evidence/keeper"
-
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -14,6 +12,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/evidence/exported"
+	"github.com/cosmos/cosmos-sdk/x/evidence/keeper"
 	"github.com/cosmos/cosmos-sdk/x/evidence/types"
 )
 
