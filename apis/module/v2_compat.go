65295e9fbf136286cd28483f16f1acbcee77cf2c
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-04-20 09:22:22 +0200

    add: missing files

--- apis/module/v2_compat.go
+++ apis/module/v2_compat.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/pkg/protohelpers"
 )
 
+// this registers the extension to the protov2 registry so that we can reflect it
 func init() {
 	proto.RegisterExtension(protohelpers.GogoProtoXtToProtoXt(E_Internal))
 	proto.RegisterFile("apis/module/module.proto", fileDescriptor_4b6f5bff79d8037b)
