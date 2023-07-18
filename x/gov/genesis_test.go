6bf665fbaaacf3eaaf686c1b449db1630942d019
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2023-07-18 18:52:25 +0200

    wip: TestInitGenesis

--- x/gov/genesis_test.go
+++ x/gov/genesis_test.go
@@ -1,15 +1,23 @@
 package gov_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"cosmossdk.io/collections"
 	sdkmath "cosmossdk.io/math"
 
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/cosmos/cosmos-sdk/x/gov"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 )
 
 func TestImportExportQueues_ErrorUnconsistentState(t *testing.T) {
@@ -36,4 +44,215 @@ func TestImportExportQueues_ErrorUnconsistentState(t *testing.T) {
 	genState, err := gov.ExportGenesis(ctx, suite.GovKeeper)
 	require.NoError(t, err)
 	require.Equal(t, genState, v1.DefaultGenesisState())
+}
+
+func TestInitGenesis(t *testing.T) {
+	var (
+		testAddrs = simtestutil.CreateRandomAccounts(2)
+		params    = &v1.Params{
+			MinDeposit: sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdkmath.NewInt(42))),
+		}
+		depositAmount = sdk.Coins{
+			sdk.NewCoin(
+				"stake",
+				sdkmath.NewInt(1234),
+			),
+		}
+		deposits = v1.Deposits{
+			{
+				ProposalId: 1234,
+				Depositor:  testAddrs[0].String(),
+				Amount:     depositAmount,
+			},
+			{
+				ProposalId: 1234,
+				Depositor:  testAddrs[1].String(),
+				Amount:     depositAmount,
+			},
+		}
+		votes = []*v1.Vote{
+			{
+				ProposalId: 1234,
+				Voter:      testAddrs[0].String(),
+				Options:    v1.NewNonSplitVoteOption(v1.OptionYes),
+			},
+			{
+				ProposalId: 1234,
+				Voter:      testAddrs[1].String(),
+				Options:    v1.NewNonSplitVoteOption(v1.OptionNo),
+			},
+		}
+		msgs, _        = sdktx.SetMsgs([]sdk.Msg{mkTestLegacyContent(t)})
+		depositEndTime = time.Now().Add(time.Hour * 8)
+		votingEndTime  = time.Now().Add(time.Hour * 24)
+		proposals      = []*v1.Proposal{
+			{
+				Id:             1234,
+				Messages:       msgs,
+				Status:         v1.StatusVotingPeriod,
+				DepositEndTime: &depositEndTime,
+				VotingEndTime:  &votingEndTime,
+			},
+			{
+				Id:             12345,
+				Messages:       msgs,
+				Status:         v1.StatusDepositPeriod,
+				DepositEndTime: &depositEndTime,
+				VotingEndTime:  &votingEndTime,
+			},
+		}
+		assertProposals = func(t *testing.T, ctx sdk.Context, s suite, expectedProposals []*v1.Proposal) {
+			assert := assert.New(t)
+			require := require.New(t)
+			it, err := s.GovKeeper.Proposals.Iterate(ctx, nil)
+			require.NoError(err)
+			ps, err := it.Values()
+			require.NoError(err)
+			var expectedPs []v1.Proposal // turn []*v1.Proposal to []v1.Proposal for assertion
+			for _, p := range expectedProposals {
+				expectedPs = append(expectedPs, *p)
+			}
+			assert.Equal(expectedPs[0], ps[0], "XXX") // TIME DOESN"T WORK!
+			return
+			assert.ElementsMatch(expectedPs, ps)
+			for _, p := range expectedProposals {
+				fmt.Println("ASSERT", p.Status)
+				switch p.Status {
+				case v1.StatusVotingPeriod:
+					assert.True(s.GovKeeper.ActiveProposalsQueue.Has(ctx, collections.Join(*p.VotingEndTime, p.Id)))
+					assert.False(s.GovKeeper.InactiveProposalsQueue.Has(ctx, collections.Join(*p.DepositEndTime, p.Id)))
+					assert.True(s.GovKeeper.VotingPeriodProposals.Has(ctx, p.Id))
+				case v1.StatusDepositPeriod:
+					assert.False(s.GovKeeper.ActiveProposalsQueue.Has(ctx, collections.Join(*p.VotingEndTime, p.Id)))
+					assert.True(s.GovKeeper.InactiveProposalsQueue.Has(ctx, collections.Join(*p.DepositEndTime, p.Id)))
+					assert.False(s.GovKeeper.VotingPeriodProposals.Has(ctx, p.Id))
+				}
+			}
+		}
+	)
+
+	tests := []struct {
+		name          string
+		genesis       v1.GenesisState
+		moduleBalance sdk.Coins
+		requirePanic  bool
+		assert        func(*testing.T, sdk.Context, suite)
+	}{
+		{
+			name:         "fail: genesis without params",
+			requirePanic: true,
+		},
+		{
+			name: "ok: genesis with only params",
+			genesis: v1.GenesisState{
+				Params: params,
+			},
+			assert: func(t *testing.T, ctx sdk.Context, s suite) {
+				p, err := s.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, *params, p)
+			},
+		},
+		{
+			name: "ok: genesis with constitution",
+			genesis: v1.GenesisState{
+				Params:       params,
+				Constitution: "my constitution",
+			},
+			assert: func(t *testing.T, ctx sdk.Context, s suite) {
+				p, err := s.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, *params, p)
+				c, err := s.GovKeeper.Constitution.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, "my constitution", c)
+			},
+		},
+		{
+			name:          "fail: genesis with deposits but module balance is not equal to total deposits",
+			moduleBalance: depositAmount,
+			genesis: v1.GenesisState{
+				Params:   params,
+				Deposits: deposits,
+			},
+			requirePanic: true,
+		},
+		{
+			name:          "ok: genesis with deposits and module balance is equal to total deposits",
+			moduleBalance: depositAmount.MulInt(sdkmath.NewInt(2)), // *2 because there's 2 deposits
+			genesis: v1.GenesisState{
+				Params:   params,
+				Deposits: deposits,
+			},
+			assert: func(t *testing.T, ctx sdk.Context, s suite) {
+				p, err := s.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, *params, p)
+				ds, err := s.GovKeeper.GetDeposits(ctx, deposits[0].ProposalId)
+				require.NoError(t, err)
+				assert.ElementsMatch(t, deposits, ds)
+			},
+		},
+		{
+			name: "ok: genesis with votes",
+			genesis: v1.GenesisState{
+				Params: params,
+				Votes:  votes,
+			},
+			assert: func(t *testing.T, ctx sdk.Context, s suite) {
+				p, err := s.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, *params, p)
+				rng := collections.NewPrefixedPairRange[uint64, sdk.AccAddress](1234)
+				it, err := s.GovKeeper.Votes.Iterate(ctx, rng)
+				require.NoError(t, err)
+				vs, err := it.Values()
+				require.NoError(t, err)
+				var expectedVotes []v1.Vote // turn []*v1.Vote to []v1.Vote for assertion
+				for _, v := range votes {
+					expectedVotes = append(expectedVotes, *v)
+				}
+				assert.ElementsMatch(t, expectedVotes, vs)
+			},
+		},
+		{
+			name: "ok: genesis with proposals",
+			genesis: v1.GenesisState{
+				Params:    params,
+				Proposals: proposals,
+			},
+			assert: func(t *testing.T, ctx sdk.Context, s suite) {
+				p, err := s.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				assert.Equal(t, *params, p)
+				assertProposals(t, ctx, s, proposals)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			suite := createTestSuite(t)
+			app := suite.App
+			ctx := app.BaseApp.NewContext(false)
+			if tt.moduleBalance.IsAllPositive() {
+				err := suite.BankKeeper.MintCoins(ctx, minttypes.ModuleName, tt.moduleBalance)
+				require.NoError(t, err)
+				err = suite.BankKeeper.SendCoinsFromModuleToModule(ctx, minttypes.ModuleName, govtypes.ModuleName, tt.moduleBalance)
+				require.NoError(t, err)
+			}
+			if tt.requirePanic {
+				defer func() {
+					require.NotNil(t, recover())
+				}()
+			}
+
+			gov.InitGenesis(ctx, suite.AccountKeeper, suite.BankKeeper, suite.GovKeeper, &tt.genesis)
+
+			if tt.requirePanic {
+				require.Fail(t, "should have panic")
+				return
+			}
+			tt.assert(t, ctx, suite)
+		})
+	}
 }
