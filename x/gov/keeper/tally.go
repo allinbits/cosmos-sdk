69bd38113caf9dcf9cb9a093f9f272ff820d213b
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2023-07-20 20:01:48 +0200

    fix HasReachedQuorum compilation error

--- x/gov/keeper/tally.go
+++ x/gov/keeper/tally.go
@@ -206,7 +206,7 @@ func (keeper Keeper) HasReachedQuorum(ctx context.Context, proposal v1.Proposal)
 
 		// iterate over all delegations from voter, deduct from any delegated-to validators
 		err = keeper.sk.IterateDelegations(ctx, voter, func(index int64, delegation stakingtypes.DelegationI) (stop bool) {
-			valAddrStr := delegation.GetValidatorAddr().String()
+			valAddrStr := delegation.GetValidatorAddr()
 
 			if val, ok := currValidators[valAddrStr]; ok {
 				// There is no need to handle the special case that validator address equal to voter address.
